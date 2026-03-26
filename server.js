7f52e89b4ae6e3f643ad0292508817ab419fbf19
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2026-03-26 12:36:08 +0530

    feat: add interactive chat loop with continuous user input
    Introduce new inputChatLoop module that allows continuous conversation with the model until the user types "exit". This replaces the previous single-input approach with a more interactive experience.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import chat from "./src/chat/chat.js";
 import inputChat from "./src/chat/inputChat.js";
+import inputChatLoop from "./src/chat/inputChatLoop.js";
 
 // chat();
-inputChat();
+// inputChat();
+inputChatLoop();
